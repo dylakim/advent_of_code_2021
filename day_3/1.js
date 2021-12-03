2360f037096176746ce0281281a3bcf17204290f
Kim Dyla <kim.dyla@helloworld.com>
2021-12-03 14:03:07 -0500

    day 3 pt 1

--- day_3/1.js
+++ day_3/1.js
@@ -2,6 +2,35 @@ const shared = require('../shared');
 
 const data = shared.splitLines(shared.readFile(__dirname));
 
-// put stuff here
+const decodeArray = [];
+let gammaRateBinary = '';
+let epsilonRateBinary = '';
 
-// console.log(answer);
+data.forEach((line) => {
+    for (let i = 0; i < line.length; i++) {
+        const currBitArr = decodeArray[i];
+        if (!currBitArr) decodeArray.push([]);
+        const bit = line.charAt(i);
+        decodeArray[i].push(bit);
+    }
+});
+
+decodeArray.forEach((bitArr) => {
+    const numOfZeros = bitArr.reduce((acc, curr) => {
+        if (curr === '0') acc += 1;
+        return acc;
+    }, 0);
+
+    gammaRateBinary += (bitArr.length / 2) > numOfZeros ? 1 : 0;
+    epsilonRateBinary += (bitArr.length / 2) < numOfZeros ? 1 : 0;
+});
+
+const gammaRateDecimal = convertBinaryToDecimal(gammaRateBinary);
+const epsilonRateDecimal = convertBinaryToDecimal(epsilonRateBinary);
+
+const powerConsumption = gammaRateDecimal * epsilonRateDecimal;
+console.log(powerConsumption);
+
+function convertBinaryToDecimal(binary) {
+    return parseInt(binary, 2);
+}
